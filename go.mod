d1989f373fb2511ab6637f2e0fb5e4f55deb859f
Smart Guy <thedefinedone@pop-os.localdomain>
2020-12-22 17:06:25 -0700

    first pass of functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/PhiBrandon/iampolicyparser
 
 go 1.14
+
+require github.com/aws/aws-sdk-go v1.36.14
--- main.go
+++ main.go
@@ -1,5 +1,68 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/url"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/iam"
+)
+
+type Data struct {
+	Version   string
+	Statement []Statement
+}
+type Statement struct {
+	Resource string
+}
+
+//func createPolicyVersionInput()
+
+func main() {
+	session := session.Must(session.NewSession())
+
+	iamClient := iam.New(session)
+
+	//https://docs.aws.amazon.com/sdk-for-go/api/service/iam/#IAM.ListGroups
+
+	lgOutput, err := iamClient.ListGroups(&iam.ListGroupsInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, group := range lgOutput.Groups {
+		lagpOutput, err := iamClient.ListAttachedGroupPolicies(&iam.ListAttachedGroupPoliciesInput{
+			GroupName: group.GroupName,
+		})
+		if err != nil {
+			log.Println(err)
+		}
+		for _, policy := range lagpOutput.AttachedPolicies {
+			gpoutput, err := iamClient.GetPolicy(&iam.GetPolicyInput{
+				PolicyArn: policy.PolicyArn,
+			})
+			if err != nil {
+				log.Println(err)
+			}
+			pv, _ := iamClient.GetPolicyVersion(&iam.GetPolicyVersionInput{
+				VersionId: gpoutput.Policy.DefaultVersionId,
+				PolicyArn: gpoutput.Policy.Arn,
+			})
+
+			decodedv, err := url.QueryUnescape(aws.StringValue(pv.PolicyVersion.Document))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			aData := Data{}
+			err = json.Unmarshal([]byte(decodedv), &aData)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(*policy.PolicyName)
+			fmt.Println(aData.Statement)
+		}
+	}
 }
